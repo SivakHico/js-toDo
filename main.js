68125e0578d5acf10d43879c8e8d14312eb3098d
Sivak Hico <strukggg@gmail.com>
2022-09-13 16:06:57 +0200

    564

--- main.js
+++ main.js
@@ -21,6 +21,36 @@ document.querySelector("#myUL").innerHTML = htmlList;
 
 console.log(htmlList);
 
+
+function newElement() {
+    let li = document.createElement("li");
+    let inputValue = document.getElementById("myInput").value;
+    let t = document.createTextNode(inputValue);
+    li.appendChild(t);
+    if (inputValue === "") {
+      alert("You must write something!");
+    } else {
+      document.getElementById("myUL").appendChild(li);
+    }
+    document.getElementById("myInput").value = "";
+  
+    let span = document.createElement("SPAN");
+    let txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    li.appendChild(span);
+  
+    for (i = 0; i < close.length; i++) {
+      close[i].onclick = function () {
+        let div = this.parentElement;
+        div.style.display = "none";
+      };
+    }
+  }
+  
+
+
+
 let myNodelist = document.getElementsByTagName("LI");
 
 for (let i = 0; i < myNodelist.length; i++) {
@@ -49,29 +79,3 @@ list.addEventListener(
   },
   false
 );
-
-function newElement() {
-  let li = document.createElement("li");
-  let inputValue = document.getElementById("myInput").value;
-  let t = document.createTextNode(inputValue);
-  li.appendChild(t);
-  if (inputValue === "") {
-    alert("You must write something!");
-  } else {
-    document.getElementById("myUL").appendChild(li);
-  }
-  document.getElementById("myInput").value = "";
-
-  let span = document.createElement("SPAN");
-  let txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  li.appendChild(span);
-
-  for (i = 0; i < close.length; i++) {
-    close[i].onclick = function () {
-      let div = this.parentElement;
-      div.style.display = "none";
-    };
-  }
-}
