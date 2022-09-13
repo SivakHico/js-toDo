bdef0ec94c678c3bf6a8c20e3d93ee8c1ee694d8
Sivak Hico <strukggg@gmail.com>
2022-09-13 16:32:24 +0200

    last edition

--- main.js
+++ main.js
@@ -21,36 +21,6 @@ document.querySelector("#myUL").innerHTML = htmlList;
 
 console.log(htmlList);
 
-
-function newElement() {
-    let li = document.createElement("li");
-    let inputValue = document.getElementById("myInput").value;
-    let t = document.createTextNode(inputValue);
-    li.appendChild(t);
-    if (inputValue === "") {
-      alert("You must write something!");
-    } else {
-      document.getElementById("myUL").appendChild(li);
-    }
-    document.getElementById("myInput").value = "";
-  
-    let span = document.createElement("SPAN");
-    let txt = document.createTextNode("\u00D7");
-    span.className = "close";
-    span.appendChild(txt);
-    li.appendChild(span);
-  
-    for (i = 0; i < close.length; i++) {
-      close[i].onclick = function () {
-        let div = this.parentElement;
-        div.style.display = "none";
-      };
-    }
-  }
-  
-
-
-
 let myNodelist = document.getElementsByTagName("LI");
 
 for (let i = 0; i < myNodelist.length; i++) {
@@ -79,3 +49,32 @@ list.addEventListener(
   },
   false
 );
+
+function newElement() {
+  let li = document.createElement("li");
+  let inputDay = document.getElementById("weekdays").value;
+  let inputValue = document.getElementById("myInput").value;
+  let input = document.createTextNode(inputValue);
+  let day = document.createTextNode(inputDay);
+  li.appendChild(input);
+  li.appendChild(day);
+  if (inputValue === '' || inputDay === '') {
+    alert("You must write something!");
+  } else {
+    document.getElementById("myUL").appendChild(li);
+  }
+  document.getElementById("myInput").value = "";
+
+  let span = document.createElement("SPAN");
+  let txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function () {
+      let div = this.parentElement;
+      div.style.display = "none";
+    };
+  }
+}
