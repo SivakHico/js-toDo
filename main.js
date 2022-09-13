960a850dca1e29a9f4ea0866cfba81db972145bc
Sivak Hico <strukggg@gmail.com>
2022-09-13 15:24:17 +0200

    first step to do

---
+++ main.js
@@ -0,0 +1,77 @@
+let toDoList = [
+  "wash the kitchen",
+  "do the laundry",
+  "walk the dog",
+  "call my lawyer",
+  "be good at programming",
+  "make a to do list",
+];
+
+const intoHTML = (item) => {
+  return `
+      <li class="list-item">
+        ${item}
+      </li>
+    `;
+};
+
+const htmlList = toDoList.map(intoHTML).join("");
+
+document.querySelector("#myUL").innerHTML = htmlList;
+
+console.log(htmlList);
+
+let myNodelist = document.getElementsByTagName("LI");
+
+for (let i = 0; i < myNodelist.length; i++) {
+  let span = document.createElement("SPAN");
+  let txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
+
+let close = document.getElementsByClassName("close");
+for (let i = 0; i < close.length; i++) {
+  close[i].onclick = function () {
+    let div = this.parentElement;
+    div.style.display = "none";
+  };
+}
+
+let list = document.querySelector("ul");
+list.addEventListener(
+  "click",
+  function (ev) {
+    if (ev.target.tagName === "LI") {
+      ev.target.classList.toggle("checked");
+    }
+  },
+  false
+);
+
+function newElement() {
+  let li = document.createElement("li");
+  let inputValue = document.getElementById("myInput").value;
+  let t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === "") {
+    alert("You must write something!");
+  } else {
+    document.getElementById("myUL").appendChild(li);
+  }
+  document.getElementById("myInput").value = "";
+
+  let span = document.createElement("SPAN");
+  let txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function () {
+      let div = this.parentElement;
+      div.style.display = "none";
+    };
+  }
+}
